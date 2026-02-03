f2b984d684867f17b511880d94550246835435c7
stonelv <zhe.lv.82@gmail.com>
2026-02-03 16:19:09 +0800

    task9-by-kimi2.5-v3

--- config.py
+++ config.py
@@ -7,15 +7,24 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # 允许多线程访问 SQLite
     SQLALCHEMY_ENGINE_OPTIONS = {
-        'connect_args': {'check_same_thread': False}
+        'connect_args': {'check_same_thread': False},
+        'pool_pre_ping': True,
+        'pool_recycle': 300
     }
 
 
 class TestingConfig(Config):
     TESTING = True
     # 使用文件数据库而不是内存数据库，以支持多线程测试
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_seckill.db'
+    # 添加超时和隔离级别配置，减少 database is locked 错误
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test_seckill.db?timeout=30&mode=wal'
     WTF_CSRF_ENABLED = False
     SQLALCHEMY_ENGINE_OPTIONS = {
-        'connect_args': {'check_same_thread': False}
+        'connect_args': {
+            'check_same_thread': False,
+            'timeout': 30  # 增加超时时间，等待锁释放
+        },
+        'pool_pre_ping': True,
+        'pool_recycle': 300,
+        'isolation_level': 'SERIALIZABLE'  # 最高隔离级别
     }
